2fd1a4870bf403af8c16ab9d5341e5c6299f9dd1
Juliani Schlickmann <juliani@thinkific.com>
2022-08-24 14:21:05 -0700

    Update specifics

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -107,6 +107,12 @@ source $ZSH/oh-my-zsh.sh
 alias zshconfig="code ~/.zshrc"
 alias sourcezsh="source ~/.zshrc"
 
+# Thinkific specific
+alias think="code ~/Thinkific/workspace/thinkific-dev"
+alias communities="code ~/Thinkific/workspace/communities-dev"
+alias graphdump="bin/rake graphql:schema_dump"
+alias startAWS="aws --profile think-dev sso login"
+
 include "$DOTFILES/zsh/pure.zsh"
 
 if [[ $(uname) = Darwin ]]; then
@@ -114,3 +120,26 @@ if [[ $(uname) = Darwin ]]; then
   include "$DOTFILES/zsh/thinkific.zsh"
   include "$DOTFILES/zsh/fzf.zsh"
 fi
+
+
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
